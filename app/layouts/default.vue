de23ee9c192493a435df27aa11f21a1decdf780d
devsrijit <srijitghosh7331@gmail.com>
2024-08-20 01:05:07 +0530

    feat: Add @vercel/speed-insights package for performance optimization

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+</script>
+
 <template>
+  <SpeedInsights />
   <div>
     <div>
       <LayoutNavbar class="fixed bottom-0 z-50 flex sm:bottom-auto sm:top-0" />
