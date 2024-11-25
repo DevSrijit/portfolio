2b5eebd43f1cb9775ec52f42401060605f8bfc72
Srijit Ghosh <srijitghosh7331@gmail.com>
2024-11-26 00:32:55 +0530

    [FEAT]: Improved SEO on the website.

--- app/app.vue
+++ app/app.vue
@@ -32,22 +32,65 @@ useHead({
       href: '/site.webmanifest',
     },
   ],
+  title: 'Srijit Ghosh - Developer & Technologist',
+  meta: [
+    {
+      name: 'description',
+      content: 'Srijit Ghosh is a student, developer, and designer. He loves to build things that make a difference.',
+    },
+    {
+      name: 'keywords',
+      content: 'srijit, srijit ghosh, srijit ghosh developer, srijit developer, srijit ghosh coder, srijit coder, srijit ghosh teenager, srijit teenager, srijit .life, srijit ghosh .life, teenager srijit, teenager srijit ghosh, srijit hackclub, srijit ghosh hackclub, srijit ghosh hackclubber, srijit hackclubber, srijit codeday, srijit ghosh codeday, srijit codeday kolkata, srijit ghosh codeday kolkata, codeday kolkata, codeday, hackclub, the code cafe, the code cafe leaders, the code cafe hackclub, the code cafe founder',
+    },
+    {
+      name: 'robots',
+      content: 'index, follow',
+    },
+    {
+      httpEquiv: 'Content-Type',
+      content: 'text/html; charset=utf-8',
+    },
+    {
+      name: 'language',
+      content: 'English',
+    },
+    {
+      name: 'author',
+      content: 'Srijit Ghosh',
+    },
+    {
+      property: 'og:title',
+      content: 'Srijit Ghosh - Developer & Technologist',
+    },
+    {
+      property: 'og:description',
+      content: 'Srijit Ghosh is a student, developer, and designer. He loves to build things that make a difference.',
+    },
+    {
+      property: 'og:image',
+      content: 'https://cloud-fx6bgjeux-hack-club-bot.vercel.app/0portfolio.png',
+    },
+    {
+      property: 'og:url',
+      content: 'https://srijit.co',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+  ],
 })
 
 useSeoMeta({
-  author: 'Srijit Ghosh',
-  description: 'This is Srijit\'s personal website. Srijit is a student, developer, hardware lover and certified geek. He loves to build things that make a difference.',
-  ogType: 'website',
-  ogTitle: appConfig.appName,
-  ogSiteName: 'Srijit&apos;s Personal Website',
-  ogDescription: 'This is Srijit Ghosh\'s personal website. Srijit is a student, developer, and designer. He loves to build things that make a difference.',
-  twitterDescription: 'This is Srijit Ghosh\'s personal website. Srijit is a student, developer, and designer. He loves to build things that make a difference.',
-  ogUrl: () => runtimeConfig.public.siteUrl,
-  ogLocale: () => locale.value,
-  twitterTitle: appConfig.appName,
+  title: 'Srijit Ghosh - Developer & Technologist',
+  ogTitle: 'Srijit Ghosh - Developer & Technologist',
+  description: 'Srijit Ghosh is a student, developer, and designer. He loves to build things that make a difference.',
+  ogDescription: 'Srijit Ghosh is a student, developer, and designer. He loves to build things that make a difference.',
+  ogImage: 'https://cloud-fx6bgjeux-hack-club-bot.vercel.app/0portfolio.png',
+  ogUrl: 'https://srijit.co',
   twitterCard: 'summary_large_image',
-  twitterSite: appConfig.twitterUsername,
-  twitterCreator: appConfig.twitterUsername,
+  twitterTitle: 'Srijit Ghosh - Developer & Technologist',
+  twitterDescription: 'Srijit Ghosh is a student, developer, and designer. He loves to build things that make a difference.',
 })
 </script>
 
